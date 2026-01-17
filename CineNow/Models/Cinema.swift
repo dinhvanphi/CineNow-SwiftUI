0afa7bc44c1a91cd4a9f07ec045ea1f838e24152
dinhvanphi <dinhvanphi01478965@gmail.com>
2026-01-17 18:35:59 +0700

    create : add cinema model

--- CineNow/Models/Cinema.swift
+++ CineNow/Models/Cinema.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+struct Cinema : Identifiable , Codable {
+    let id : UUID
+    let name : String
+    let address : String
+    let city : String
+    
+    enum CodingKeys :String , CodingKey {
+        case id
+        case name
+        case address
+        case city 
+    }
+}
