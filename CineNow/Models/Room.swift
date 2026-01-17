9d655205c6edb8ccdf3e56890f38e63187ad0c44
dinhvanphi <dinhvanphi01478965@gmail.com>
2026-01-17 19:36:23 +0700

    create : add room model

--- CineNow/Models/Room.swift
+++ CineNow/Models/Room.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+struct Room : Identifiable , Codable {
+    let id : UUID
+    let cinemaId : UUID
+    let name : String
+    var capacity : Int
+    
+    enum CodingKeys : String , CodingKey {
+        case name
+        case capacity
+        case id = "room_id"
+        case cinemaId = "cinama_id"
+    }
+}
