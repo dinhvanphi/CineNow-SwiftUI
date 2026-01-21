c7cd000b52009e65cf56324b3507bf7826419511
dinhvanphi <dinhvanphi01478965@gmail.com>
2026-01-21 22:55:49 +0700

    add : SignUpViewModel

---
+++ CineNow/View/AccountVerificationView.swift
@@ -0,0 +1,15 @@
+//
+//  AccountVerificationView.swift
+//  CineNow
+//
+//  Created by Đinh Văn Phi on 21/1/26.
+//
+
+import Foundation
+import SwiftUI
+
+struct AccountVerificationView : View {
+    var body: some View {
+        
+    }
+}
--- CineNow/ViewModel/SignUpViewModel.swift
+++ CineNow/ViewModel/SignUpViewModel.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+import Combine
+
+final class SignUpViewModel : ObservableObject {
+    @Published var name = ""
+    @Published var email = ""
+    @Published var phone = ""
+    @Published var password = ""
+    @Published var isLoading = false
+    @Published var errorMessage : String?
+    @Published var shouldNavigatetoOTP = false
+    @Published var tempUser : User?
+    
+    func register (){
+        guard !name.isEmpty ,
+              !email.isEmpty ,
+              !phone.isEmpty ,
+              !password.isEmpty else {
+            errorMessage = "Vui lòng nhập đầy đủ thông tin"
+            return
+        }
+        isLoading = true
+        errorMessage = nil
+        
+        AuthService.shared.register(
+            name : name ,
+            email : email ,
+            phone : phone ,
+            password : password
+        ) {[weak self] result in
+            guard let self = self else {
+                return
+            }
+            self.isLoading = false
+            
+            switch result {
+            case .success(let response) :
+                if response.success , let registerUser = response.user {
+                    let user = User(from : registerUser)
+                    self.tempUser = user
+                    self.shouldNavigatetoOTP = true
+                } else {
+                    self.errorMessage = response.message
+                }
+            case.failure(let error) :
+                self.errorMessage = error.localizedDescription
+            }
+        }
+    }
+}
