9d7d1978fdc5e7529b14faa691795651507fa899
dinhvanphi <dinhvanphi01478965@gmail.com>
2026-01-23 18:40:10 +0700

    feat : account verify service

--- CineNow/Services/Auth/AccountVerificationService.swift
+++ CineNow/Services/Auth/AccountVerificationService.swift
@@ -23,5 +23,40 @@ final class AccountVerificationService {
 
         var request = URLRequest(url : url)
         request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let body : [String :Any] = [
+            "email" : email ,
+            "otp" : otp
+        ]
+        request.httpBody = try? JSONSerialization.data(withJSONObject: body)
+        URLSession.shared.dataTask(with: request) { data , response , error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            guard let data = data ,
+                  let httpResponse = response as? HTTPURLResponse
+            else {
+                DispatchQueue.main.async {
+                    completion(.failure(URLError(.badServerResponse)))
+                }
+                return
+            }
+            DispatchQueue.main.async {
+                do {
+                    let decoded = try JSONDecoder().decode(
+                        VerifyAccountResponse.self,
+                        from: data
+                    )
+                    completion(.success(decoded))
+                } catch {
+                    completion(.failure(error))
+                }
+            }
+           
+        }.resume()
     }
 }
