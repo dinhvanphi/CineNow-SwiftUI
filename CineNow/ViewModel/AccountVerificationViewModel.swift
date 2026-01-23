5e04f08e75e5ee162b18b070052fc192a31fdecf
dinhvanphi <dinhvanphi01478965@gmail.com>
2026-01-23 18:41:27 +0700

    feat : account verify VM

--- CineNow/ViewModel/AccountVerificationViewModel.swift
+++ CineNow/ViewModel/AccountVerificationViewModel.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import Combine
+
+public class AccountVerificationViewModel : ObservableObject {
+    @Published var otp = ""
+    
+    @Published var isLoading = false
+    @Published var errorMessage : String?
+    @Published var isVerified = false
+    
+    let email : String
+    init (email : String) {
+        self.email = email
+    }
+    func verifyAccount () {
+        guard otp.count == 6 else {
+            errorMessage = "Mã OTP phải gồm 6 chữ số"
+            return
+        }
+        isLoading = true
+        errorMessage = nil
+        AccountVerificationService.shared.verifyAccount(
+            email: email,
+            otp: otp
+        ){
+            [weak self] result in
+            guard let self =  self else {
+                return
+            }
+            self.isLoading = false
+            
+            switch result {
+            case .success(let response) :
+                if response.success {
+                    if let token = response.token{
+                        TokenManager.shared.save(token)
+                    }
+                    self.isVerified = true
+                } else {
+                    self.errorMessage = response.message
+                }
+            case .failure(let error) :
+                self.errorMessage = error.localizedDescription
+            }
+        }
+    }
+}
