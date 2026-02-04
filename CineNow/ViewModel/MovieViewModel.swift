61f0c5d1a19cf527dec0d6889863e1b871f0cfd3
dinhvanphi <dinhvanphi01478965@gmail.com>
2026-02-04 18:36:42 +0700

    feat : add movies view model

--- CineNow/ViewModel/MovieViewModel.swift
+++ CineNow/ViewModel/MovieViewModel.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import Combine
+
+final class MovieViewModel : ObservableObject {
+    @Published var movies : [Movie] = []
+    @Published var isLoading = false
+    @Published var errorMessage : String?
+    
+    var featuredMovies : [Movie]{
+        movies.filter{$0.featured}
+    }
+    var nowShowingMovies : [Movie] {
+        movies.filter{$0.status == "now_showing"}
+    }
+    var comingSoonMovies : [Movie]{
+        movies.filter{$0.status == "coming_soon"}
+    }
+    
+    func loadMovies () {
+        isLoading = true
+        
+        MovieService.shared.fetchMovies {[weak self ] result in
+            guard let self = self else {
+                return
+            }
+            self.isLoading = false
+            
+            switch result {
+            case.success(let movies) :
+                self.movies = movies
+                
+            case.failure(let error) :
+                self.errorMessage = error.localizedDescription
+            }
+        }
+    }
+}
